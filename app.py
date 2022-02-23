20d64250935506942ca8cd7c4fdf8a6823167bab
orange <nicklin19930509@gmail.com>
2022-02-23 23:56:44 +0800

    update

--- app.py
+++ app.py
@@ -1190,11 +1190,3 @@ def Postback_game(event):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
