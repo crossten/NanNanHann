68a6d3d7585944feb3e43a532cd1cb4ae733317d
crossten <100950020+crossten@users.noreply.github.com>
2022-03-05 02:41:52 +0800

    Update _account.py

--- _account.py
+++ _account.py
@@ -1,19 +1,19 @@
 def line_token():
-    return '5J0Yd9JrBmvmKYQAxzBxxPih5UwIsnhq10XrV3cuHtT73wc39VyPfaqAg4mYBYJ9j42mCksb59asOAPrKD8CqtZfcb91jKAVcizOnSyuqQ89DORIyr1jytPlgEI6w2YBxr0LAw4fux88D0CM910cTwdB04t89/1O/w1cDnyilFU='
+    return ''
 
 def line_secret():
-    return '08db777bef6bdcb15bc4508169a278b0'
+    return ''
 
 def admin():
     return []
 
 def imgur():
-    return '7cfa5be8b3677c3'
+    return ''
 
 def redis(dict):
     data = {
-        'host' : 'redis-19700.c263.us-east-1-2.ec2.cloud.redislabs.com',
-        'port' : '19700',
-        'password' : 'ErfkscpLFvZMXJOVBCFQ4ClRF9Ruq4VD'
+        'host' : '',
+        'port' : '',
+        'password' : ''
     }
     return data[dict]
